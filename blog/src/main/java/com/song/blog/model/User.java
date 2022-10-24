4a2c21502371e7ed2dd36c21e98efbf5d3ea0213
yahoo <yahoo@LAPTOP-1HVBBQLT>
2022-10-25 00:09:00 +0900

    2022년 10월 25일 insert 및 update 테스트

--- blog/src/main/java/com/song/blog/model/User.java
+++ blog/src/main/java/com/song/blog/model/User.java
@@ -4,12 +4,15 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,6 +26,7 @@ import lombok.NoArgsConstructor;
 @Builder	// 빌더 패턴!!!
 //ORM : Java(및 다른언어) Object를 테이블에 매핑해주는 기술
 @Entity	// User 클래스가 MySQL에 테이블이 생성이 된다.
+// @DynamicInsert // insert시에 null인 필드를 제외시켜준다.
 public class User {
 	
 	@Id	// Primary key
@@ -38,8 +42,10 @@ public class User {
 	@Column(nullable = false,length = 50)
 	private String email;
 	
-	@ColumnDefault("'user'")					// @ColumnDefault() 가로 사이의 문자값을 줄때는 "" 안에 ''를 넣어주어야 한다
-	private String role;	// Enum을 쓰는게 좋다
+	//@ColumnDefault("'user'")					// @ColumnDefault() 가로 사이의 문자값을 줄때는 "" 안에 ''를 넣어주어야 한다
+	// DB는 RoleType이라는게 없다.
+	@Enumerated(EnumType.STRING)
+	private RoleType role;	// Enum을 쓰는게 좋다 // ADMIN, USER
 	
 	@CreationTimestamp	// 시간이 자동으로 입력
 	private Timestamp createDate;
---
+++ blog/src/main/java/com/song/blog/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.song.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.song.blog.model.User;
+
+// DAO
+// 자동으로 bean등록이 된다.
+// @Repository // 생략이 가능하다
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+	
+}
